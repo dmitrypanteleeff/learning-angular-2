3e1b628f80847a34e0fafc676fc593b2a37fa0fa
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2022-12-11 15:08:40 +0300

    089 Allowing the User to Add Ingredients to the Shopping List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +15,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @Output() emitAddObj = new EventEmitter<Ingredient>()
+  @Output() emitDeleteObj = new EventEmitter<Ingredient>()
+  @Output() emitClearAll = new EventEmitter<boolean>()
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    console.log(this.nameInput)
+    if (this.nameInput.nativeElement.value) {
+      this.nameInputStr = this.nameInput.nativeElement.value;
+    }
+  }
+
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  nameInputStr: string;
+  amountInputStr: number;
+  clearAll: boolean;
+  ingidientObj: Ingredient;
+
+  addFunc() {
+    this.nameInputStr = this.nameInput.nativeElement.value;
+    this.amountInputStr = this.amountInput.nativeElement.value;
+    this.ingidientObj = {
+      name: this.nameInputStr,
+      amount: this.amountInputStr
+    }
+    console.log('ingidientObj', this.ingidientObj)
+    this.emitAddObj.emit(this.ingidientObj)
+  }
+
+  deleteFunc() {
+    this.nameInputStr = this.nameInput.nativeElement.value;
+    this.amountInputStr = this.amountInput.nativeElement.value;
+    this.ingidientObj = {
+      name: this.nameInputStr,
+      amount: this.amountInputStr
+    }
+    this.emitDeleteObj.emit(this.ingidientObj)
+  }
+
+  clearFunc() {
+    this.clearAll = true;
+    this.emitClearAll.emit(this.clearAll)
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -13,9 +13,29 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Tomatoes', 10),
   ];
 
+  clearAllFlag: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addObj(addItem: Ingredient) {
+    console.log('addItem', addItem)
+    this.ingredients.push(addItem)
+  }
+
+  deleteObj(deleteItem: Ingredient) {
+    this.ingredients = this.ingredients.filter(itemArr => (itemArr.name !== deleteItem.name && itemArr.amount != deleteItem.amount))
+    console.log('deleteItem',deleteItem)
+  }
+
+  clearArr(item: boolean) {
+    this.clearAllFlag = item;
+    if (this.clearAllFlag) {
+      this.ingredients = [];
+      this.clearAllFlag = false;
+    }
   }
 
 }
